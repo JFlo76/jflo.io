526dada991ec6ba7d81775d2c78a0c4f9efa13e7
Jason Florence <jason@jflo.io>
2025-03-31 13:16:21 -0700

    Adjusting about me image animation

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -13,12 +13,8 @@ interface AboutSectionProps {
 export default function AboutSection({ aboutScroll, aboutRef }: AboutSectionProps) {
     return (
         <motion.section
-            className={`${styles.about} flex flex-col w-full h-full justify-center items-center p-20 relative`}
+            className={`${styles.about} flex flex-col w-full h-full justify-center items-center p-20 relative overflow-hidden`}
             ref={aboutRef}
-        // style={{
-        //     y: useTransform(aboutScroll, [0, 1], [100, 0]),
-        //     opacity: useTransform(aboutScroll, [0, 0.5], [0, 1]),
-        // }}
         >
             <div className={`flex flex-row items-center w-full md:w-[80vw] justify-between`}>
                 <motion.article
@@ -37,7 +33,7 @@ export default function AboutSection({ aboutScroll, aboutRef }: AboutSectionProp
                         A little about me...
                     </motion.h2>
                     <p className={`${styles.about_text}`}>
-                        With over a decade of experience in the tech industry, I specialize in bridging the gap between design and development, transforming concepts into functional, user-friendly interfaces. My unique blend of expertise in both disciplines enables me to create seamless digital experiences that are both visually compelling and technically sound. While some have referred to me as a 'unicorn' due to this rare combination of skills, I prefer to think of it simply as a deep passion for crafting intuitive and high-performing products.
+                        With over a decade of experience in the tech industry, I specialize in bridging the gap between design and development, transforming concepts into functional, user-friendly interfaces. My unique blend of expertise in both disciplines enables me to create seamless digital experiences that are both visually compelling and technically sound. While some have referred to me as a 'unicorn' due to this rare combination of skills, I prefer to think of it simply as a deep passion for crafting intuitive and high-performing products. Huzzah!
                     </p>
                     <a href='#work' className={`${styles.linkTo_work} ${robotoCondensed.className} uppercase`}>See my work</a>
                 </motion.article>
@@ -47,8 +43,7 @@ export default function AboutSection({ aboutScroll, aboutRef }: AboutSectionProp
                     alt='JFlo with a mug of coffee'
                     className='absolute bottom-0 right-0 w-1/2 md:w-1/3'
                     style={{
-                        y: useTransform(aboutScroll, [0.4, 0.9], [200, 0]),
-                        opacity: useTransform(aboutScroll, [0.5, 0.8], [0, 1]),
+                        y: useTransform(aboutScroll, [0.5, 0.85], [900, 0]),
                     }}
                 />
             </div>
